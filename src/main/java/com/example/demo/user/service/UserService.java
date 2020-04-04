99a509b823f836f8cb988f167ebea7a01c19cad1
BigBoos-yj <1627968953@qq.com>
2020-04-04 22:56:28 +0800

    建库脚本

--- src/main/java/com/example/demo/user/service/UserService.java
+++ src/main/java/com/example/demo/user/service/UserService.java
@@ -1,6 +1,9 @@
 package com.example.demo.user.service;
 
 import com.example.demo.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -14,7 +17,10 @@ import java.util.List;
  * @version 4.0.0.0
  * @date 2020-04-03
  */
-public interface UserService {
+public interface UserService   extends JpaRepository<User,String> {
 
-    List<User>  findUser();
+    List<User>  findUser(String userid);
+
+    @Query(value = "select r.username from sys_ser u where u.userName = :userName")
+    List<String> queryUserOwnedRoleCodes(@Param(value = "userName") String userName);
 }
