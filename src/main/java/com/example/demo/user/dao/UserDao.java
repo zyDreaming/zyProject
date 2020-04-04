152cefd1a8012be8f43951d842863e7f90e7da28
BigBoos-yj <1627968953@qq.com>
2020-04-04 22:06:17 +0800

    建库脚本

--- src/main/java/com/example/demo/user/dao/UserDao.java
+++ src/main/java/com/example/demo/user/dao/UserDao.java
@@ -14,40 +14,5 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public class UserDao {
-    private String userid;
-    private String loginname;
-    private String passwd;
-    private String username;
 
-    public String getUserid() {
-        return userid;
-    }
-
-    public void setUserid(String userid) {
-        this.userid = userid;
-    }
-
-    public String getLoginname() {
-        return loginname;
-    }
-
-    public void setLoginname(String loginname) {
-        this.loginname = loginname;
-    }
-
-    public String getPasswd() {
-        return passwd;
-    }
-
-    public void setPasswd(String passwd) {
-        this.passwd = passwd;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
 }
